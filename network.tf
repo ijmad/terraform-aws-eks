5588c8f22c1d111e6e9ac26c7da250d11552d5d8
Ian Maddison <ian@i.need.stronger.coffee>
2020-03-20 19:46:01 +0000

    Fix some bugs

--- network.tf
+++ network.tf
@@ -24,7 +24,7 @@ resource "aws_subnet" "nat_subnet" {
   count                   = 3
   
   vpc_id                  = aws_vpc.vpc.id
-  availability_zone       = data.aws_availability_zones.available.names[0]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
   cidr_block              = "10.0.${100 + count.index}.0/24"
   map_public_ip_on_launch = false
 
--- providers.tf
+++ providers.tf
@@ -1,3 +1,9 @@
+provider "random" {
+}
+
+provider "null" {
+}
+
 provider "aws" {
   region  = "eu-west-1"
   version = ">= 2.38.0"
@@ -7,4 +13,7 @@ data "aws_region" "current" {
 }
 
 data "aws_availability_zones" "available" {
+}
+
+data "aws_caller_identity" "current" {
 }
